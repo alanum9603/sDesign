527deb8ca8db1c3784612fded1ac6a08950cf54d
alanum9603 <alanum9603@gmail.com>
2024-11-06 10:11:53 -0500

    2024/11/06

--- main.js
+++ main.js
@@ -9,13 +9,41 @@ document.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("menu");
     const menuToogle = document.getElementById("menu-toggle");
     const menusTitle = document.querySelectorAll(".menu-title");
-    const logo = document.getElementsByClassName("logo");
+    const screenWidth = window.innerWidth;
 
-    menuToogle.addEventListener("click", () => {
-        if(menu.style.width === "260px") {
+    if (screenWidth < 1500) {
+        menusTitle.forEach(item => {
+            item.style.display = "none"
+
+        });
+        menu.style.width = "60px";
+    } else {
+        menusTitle.forEach(item => {
+            item.style.display = "flex"
+        });
+        menu.style.width = "260px"
+    }
+
+    window.addEventListener('resize', () => {
+        if (screenWidth < 1500) {
             menusTitle.forEach(item => {
                 item.style.display = "none"
-                
+
+            });
+            menu.style.width = "60px";
+        } else {
+            menusTitle.forEach(item => {
+                item.style.display = "flex"
+            });
+            menu.style.width = "260px"
+        }
+    })
+
+    menuToogle.addEventListener("click", () => {
+        if (menu.style.width === "260px") {
+            menusTitle.forEach(item => {
+                item.style.display = "none"
+
             });
             menu.style.width = "60px";
         } else {
