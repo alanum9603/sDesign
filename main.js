7253ad54d932fa75c483d727690f2925760d35e6
alanum9603 <alanum9603@gmail.com>
2024-11-07 02:43:24 -0500

    2024/11/07

--- main.js
+++ main.js
@@ -5,11 +5,60 @@
 
 
 // setupCounter(document.querySelector('#counter'))
+
+const navigateTo = (url) => {
+    history.pushState(null, null, url)
+    router();
+}
+
+const loadHTML = async (route, container) => {
+    fetch(route)
+        .then(response => {
+            if (response.ok){
+                return response.text();
+            } else {
+                throw new Error(`Error al cargar ${url}: ${response.statusText}`)
+            } 
+        })
+        .then(htmlContent => {
+            document.getElementById(container).innerHTML = htmlContent;
+        })
+}
+
+const router = async () => {
+    const routes = [
+        {path: "/", view: () => loadHTML("./src/homepage.html","container")},
+        {path: "/catalog", view: () => loadHTML("./src/catalog.html","container")},
+        {path: "/product", view: () => loadHTML("./src/product.html","container")}
+    ];
+
+    const potentialMatches = routes.map(route => ({
+        route,
+        isMatch: location.pathname === route.path,
+    }));
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if (!match) {
+        match = {route: routes[0], isMatch: true}
+    }
+
+    match.route.view();
+} 
 document.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("menu");
     const menuToogle = document.getElementById("menu-toggle");
     const menusTitle = document.querySelectorAll(".menu-title");
     const screenWidth = window.innerWidth;
+
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router()
 
     if (screenWidth < 1200) {
         menusTitle.forEach(item => {
